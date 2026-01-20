107b18f6f473f8fefe7c8c97e8ea02aac16be338
brogers5 <6869577+brogers5@users.noreply.github.com>
2026-01-20 10:56:37 -0500

    Update to 2.137.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'EXE'
-  url64bit       = 'https://download.live.ledger.com/ledger-live-desktop-2.135.2-win-x64.exe'
+  url64bit       = 'https://download.live.ledger.com/ledger-live-desktop-2.137.0-win-x64.exe'
   softwareName   = 'Ledger Wallet *'
-  checksum64     = 'c5557d46bf12bfe4f5304e31bd088a128454a95c38162e2fd63314ac88ab8c815918d2ce88616f570d1e1370dead79b46976339b5a156ce25679775cb1c6ea6a'
+  checksum64     = '4163e1e013d3771aeaa8ecff175ae78294802315786e7a801c07ec667cc2b7fb8bc0c9e3eec3e27709630c9432d8c5fd76f761321c32a039c3d296e6e4c61bc7'
   checksumType64 = 'sha512'
   silentArgs     = '/S'
   validExitCodes = @(0)
