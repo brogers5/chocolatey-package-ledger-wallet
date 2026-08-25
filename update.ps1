1ae0a4a1c0a657c7c616d6900e7b2b44739d1a0c
brogers5 <6869577+brogers5@users.noreply.github.com>
2026-08-25 18:48:47 -0400

    Use commit hash for 4.17.1 release

--- update.ps1
+++ update.ps1
@@ -77,7 +77,7 @@ function global:au_GetLatest {
 
     return @{
         Checksum64      = $checksumString
-        RefName         = '%40ledgerhq/live-desktop%40$servedVersion'
+        RefName         = '350c8c13635be79b08b0f710ad5cc3c09ff51a8a'
         SoftwareVersion = $servedVersion
         Url64           = "https://download.live.ledger.com/$($latestVersionInfo.path)"
         Version         = $servedVersion #This may change if building a package fix version
